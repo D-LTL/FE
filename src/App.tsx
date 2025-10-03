862d9731141b5fef8eecdda4d13b044e6d427175
ChangwooJ <wjdckddn99@naver.com>
2025-10-03 16:31:31 +0900

    feat: 진입 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import "./App.css";
+import LoginPage from './page/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>홈</div>} />
+        <Route path="/" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/page/LoginPage.tsx
@@ -0,0 +1,13 @@
+const LoginPage = () => {
+  return (
+    <div className="p-9 flex items-center justify-center h-screen reveal">
+      <p className="text-xs font-light leading-relaxed text-center">
+        사용자의 실제 목소리를 학습하고 이를 바탕으로 감정이 반영된 번역 음성을
+        생성함으로써, 기존의 딱딱한 기계음을 대체하고 개인화된 소통 경험을
+        제공하는 서비스 입니다.
+      </p>
+    </div>
+  );
+};
+
+export default LoginPage;
