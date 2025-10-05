7ddd111fb7a7d11f0a0994b4d4392720d7856cfe
ChangwooJ <wjdckddn99@naver.com>
2025-10-05 16:20:37 +0900

    feat: 메인페이지 타이틀 추가

---
+++ src/components/main/components/ProfileSection.tsx
@@ -0,0 +1,11 @@
+const ProfileSection = () => {
+  return (
+    <div className="w-full flex justify-end pr-6 mt-[70px]">
+      <button>
+        <img src="/profile.svg" alt="/profile_img" width={39} height={39} />
+      </button>
+    </div>
+  );
+}
+
+export default ProfileSection;
--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,10 +1,12 @@
 import MainTitle from "../components/main/components/MainTitle";
+import ProfileSection from "../components/main/components/ProfileSection";
 
 const MainPage = () => {
   return (
-    <>
+    <div className="w-screen h-screen flex flex-col items-center">
+      <ProfileSection />
       <MainTitle />
-    </>
+    </div>
   );
 }
 
