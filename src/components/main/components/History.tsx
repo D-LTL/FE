97ff95668d96ab17d94cc5a18fe04347bea30ca6
ChangwooJ <wjdckddn99@naver.com>
2025-10-05 17:08:02 +0900

    refactor: 히스토리 영역 재설정

--- src/components/main/components/History.tsx
+++ src/components/main/components/History.tsx
@@ -1,6 +1,6 @@
-const History = () => {
+const History = ({ className }: { className?: string }) => {
   return (
-    <div className="w-full h-screen flex flex-col">
+    <div className={`w-full flex flex-col ${className}`}>
       <div className="w-full px-6 py-3 text-sm font-normal border-b-2 border-[#D4D4D4]">히스토리</div>
       <div className="w-full bg-gray-300 flex flex-col items-center flex-1 overflow-hidden">
         <p className="text-xs font-normal py-3">번역 목록이 없습니다.</p>
--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -8,7 +8,7 @@ const MainPage = () => {
     <div className="relative w-screen h-screen flex flex-col items-center">
       <ProfileSection />
       <MainTitle />
-      <History />
+      <History className="flex-1" />
       <div className="absolute bottom-0 w-full h-[238px] bg-gradient-to-t from-white to-transparent" />
       <TranslationButton />
     </div>
