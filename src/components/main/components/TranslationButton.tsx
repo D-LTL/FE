bdd8230857b6f6250a90aa44c0017f6aa197fe6e
ChangwooJ <wjdckddn99@naver.com>
2025-10-05 16:54:52 +0900

    feat: 메인페이지 번역 버튼 추가

---
+++ src/components/main/components/TranslationButton.tsx
@@ -0,0 +1,9 @@
+const TranslationButton = () => {
+  return (
+    <div className="absolute bottom-[69px] z-10 w-[297px] h-[75px] mx-auto rounded-[80px] bg-[#787878] flex justify-center items-center text-white text-2xl font-semibold">
+      번역하기
+    </div>
+  );
+};
+
+export default TranslationButton;
--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,15 +1,18 @@
 import History from "../components/main/components/History";
 import MainTitle from "../components/main/components/MainTitle";
 import ProfileSection from "../components/main/components/ProfileSection";
+import TranslationButton from "../components/main/components/TranslationButton";
 
 const MainPage = () => {
   return (
-    <div className="w-screen h-screen flex flex-col items-center">
+    <div className="relative w-screen h-screen flex flex-col items-center">
       <ProfileSection />
       <MainTitle />
       <History />
+      <div className="absolute bottom-0 w-full h-[238px] bg-gradient-to-t from-white to-transparent" />
+      <TranslationButton />
     </div>
   );
-}
+};
 
 export default MainPage;
